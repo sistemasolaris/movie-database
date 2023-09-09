75e249ac272ec6128a9233a5d379f33b87e54a7f
Solaris <ovvo.system@gmail.com>
2023-09-09 15:23:01 -0300

    feat: serve media during development

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    poster = models.ImageField(upload_to="static/posters")
+    poster = models.ImageField(upload_to="posters/")
     year = models.SmallIntegerField()
 
     def __str__(self):
