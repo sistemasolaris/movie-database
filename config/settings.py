591ef64d8c4578333119ef82d651add85781746f
Solaris <ovvo.system@gmail.com>
2023-09-09 10:06:25 -0300

    feat: load enviroment variables from file

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+from dotenv import find_dotenv, load_dotenv
 from pathlib import Path
+
+# Load enviroment variables
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+  load_dotenv(ENV_FILE)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
