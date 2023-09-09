1de218ef95e3bfcb6186886d387a6e7180c73bbb
Solaris <ovvo.system@gmail.com>
2023-09-09 11:43:01 -0300

    style: format changes

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Movie
 
+
 class MovieAdmin(admin.ModelAdmin):
-  readonly_fields = ('id',)
+    readonly_fields = ("id",)
+
 
 admin.site.register(Movie, MovieAdmin)
--- api/models.py
+++ api/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Movie(models.Model):
-  title = models.CharField(max_length=100)
-  thumbnail = models.ImageField(upload_to='files/thumbnails')
-  year = models.SmallIntegerField()
+    title = models.CharField(max_length=100)
+    thumbnail = models.ImageField(upload_to="files/thumbnails")
+    year = models.SmallIntegerField()
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
