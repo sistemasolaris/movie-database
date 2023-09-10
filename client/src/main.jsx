1f659598ce3618847ea5ed97341fe28cf0084c3a
Solaris <ovvo.system@gmail.com>
2023-09-10 11:57:48 -0300

    feat: create root route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
 import App from "./routes/root/App.jsx";
 import "@fontsource/roboto";
 import "./index.css";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <Navbar />
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
