c51c6137eca002c2271540f878852a1cdd63601f
Solaris <ovvo.system@gmail.com>
2023-09-11 08:50:59 -0300

    feat: add preview boolean prop to MovieCard

--- client/src/routes/root/MovieCard.jsx
+++ client/src/routes/root/MovieCard.jsx
@@ -2,7 +2,7 @@ import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 
-function MovieCard({ id, poster, title, year }) {
+function MovieCard({ id, poster, title, year, preview = false }) {
     return (
         <li className="flex flex-col gap-2 p-4 border rounded-xl shadow-lg bg-gray-50 hover:bg-zinc-100 hover:scale-105 transition-all duration-200">
             <img
@@ -13,17 +13,21 @@ function MovieCard({ id, poster, title, year }) {
                 <span className="font-semibold">{title}</span>
                 <p className="text-sm">{year}</p>
             </div>
-            <div className="flex gap-2">
-                <button className="flex-1 border rounded-xl py-1 hover:border-transparent hover:bg-blue-500 hover:text-white transition-all duration-200">
-                    <VisibilityOutlinedIcon />
-                </button>
-                <button className="flex-1 border rounded-xl py-1 hover:border-transparent hover:bg-teal-500 hover:text-white transition-all duration-200">
-                    <EditIcon />
-                </button>
-                <button className="flex-1 border rounded-xl py-1 hover:border-transparent hover:bg-red-500 hover:text-white transition-all duration-200">
-                    <DeleteIcon />
-                </button>
-            </div>
+            {!preview ? (
+                <div className="flex gap-2">
+                    <button className="flex-1 border rounded-xl py-1 hover:border-transparent hover:bg-blue-500 hover:text-white transition-all duration-200">
+                        <VisibilityOutlinedIcon />
+                    </button>
+                    <button className="flex-1 border rounded-xl py-1 hover:border-transparent hover:bg-teal-500 hover:text-white transition-all duration-200">
+                        <EditIcon />
+                    </button>
+                    <button className="flex-1 border rounded-xl py-1 hover:border-transparent hover:bg-red-500 hover:text-white transition-all duration-200">
+                        <DeleteIcon />
+                    </button>
+                </div>
+            ) : (
+                ""
+            )}
         </li>
     );
 }
