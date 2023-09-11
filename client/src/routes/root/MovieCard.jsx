444cf0cb8c86626b2011d014fa8f72b62a617ce2
Solaris <ovvo.system@gmail.com>
2023-09-11 15:51:25 -0300

    feat: implement movie deletion

--- client/src/routes/root/MovieCard.jsx
+++ client/src/routes/root/MovieCard.jsx
@@ -3,6 +3,14 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 function MovieCard({ id, poster, title, year, preview = false }) {
+    function handleDelete() {
+        fetch(`http://127.0.0.1:8000/api/movie/${id}`, {
+            method: "DELETE",
+        })
+            .then((response) => console.log(response.json()))
+            .then(window.location.reload());
+    }
+
     return (
         <li className="flex flex-col gap-2 p-4 border rounded-xl shadow-lg bg-gray-50 hover:bg-zinc-100 hover:scale-105 transition-all duration-200">
             <img
@@ -21,7 +29,10 @@ function MovieCard({ id, poster, title, year, preview = false }) {
                     <button className="flex-1 border rounded-xl py-1 hover:border-transparent hover:bg-teal-500 hover:text-white transition-all duration-200">
                         <EditIcon />
                     </button>
-                    <button className="flex-1 border rounded-xl py-1 hover:border-transparent hover:bg-red-500 hover:text-white transition-all duration-200">
+                    <button
+                        onClick={handleDelete}
+                        className="flex-1 border rounded-xl py-1 hover:border-transparent hover:bg-red-500 hover:text-white transition-all duration-200"
+                    >
                         <DeleteIcon />
                     </button>
                 </div>
