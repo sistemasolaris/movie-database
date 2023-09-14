f00886dba9dab67f9a5b4f4521720e781f5f7ec0
Solaris <ovvo.system@gmail.com>
2023-09-14 15:50:22 -0300

    feat: add watchlist model to admin page

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Movie
+from .models import User, Movie, WatchlistEntry
 
 
 class MovieAdmin(admin.ModelAdmin):
@@ -8,3 +8,4 @@ class MovieAdmin(admin.ModelAdmin):
 
 admin.site.register(User)
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(WatchlistEntry)
