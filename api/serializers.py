2591f23374182e2cd8f64ca244757225b5f03aa6
Solaris <ovvo.system@gmail.com>
2023-09-14 15:51:42 -0300

    refactor: remove unused user serializer
    This serializer became unnecessary with the addition of the
    WatchlistEntrySerializer

--- api/serializers.py
+++ api/serializers.py
@@ -8,24 +8,6 @@ class MovieSerializer(serializers.ModelSerializer):
         fields = ("id", "title", "poster", "year")
 
 
-class UserSerializer(serializers.ModelSerializer):
-    watchlist = MovieSerializer(many=True, read_only=False)
-
-    class Meta:
-        model = User
-        fields = ("id", "username", "watchlist")
-
-    def update(self, instance, validated_data):
-        watchlist_data = validated_data.pop("watchlist")
-        instance = super(UserSerializer, self).update(instance, validated_data)
-
-        for movie_data in watchlist_data:
-            movie = Movie.objects.get(id=movie_data["id"])
-            instance.watchlist.add(movie)
-
-        return instance
-
-
 class WatchlistEntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = WatchlistEntry
