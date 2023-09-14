f44890ce7555820a7a67de78aa694238a4b7fbe7
Solaris <ovvo.system@gmail.com>
2023-09-14 16:59:54 -0300

    feat: setup simplejwt views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import MovieList, MovieDetail, WatchlistAPI
 
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("movie/", MovieList.as_view()),
     path("movie/<int:pk>", MovieDetail.as_view()),
     path("watchlist/", WatchlistAPI.as_view()),
