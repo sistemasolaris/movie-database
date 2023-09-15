ebb16c7d0e629a9cd509da289f0cf83c4cc4bad7
Solaris <ovvo.system@gmail.com>
2023-09-15 18:26:44 -0300

    fix: fix typo in UserSerializer create method

--- api/serializers.py
+++ api/serializers.py
@@ -22,7 +22,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create(
-            email=validated_data["email"], username=validated_data["usermame"]
+            email=validated_data["email"], username=validated_data["username"]
         )
         user.set_password(validated_data["password"])
         user.save()
