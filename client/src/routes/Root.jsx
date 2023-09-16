4eebce76bd9bcc0028d21ab13fb1dfffa1ffa701
Solaris <ovvo.system@gmail.com>
2023-09-16 17:12:48 -0300

    fix: stop logging response data on console

--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -8,7 +8,6 @@ function Root() {
         fetch("http://127.0.0.1:8000/api/movie/")
             .then((response) => response.json())
             .then((data) => {
-                console.log(data);
                 setMovieList(data);
             });
     }, []);
