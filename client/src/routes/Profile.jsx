e3d2d6930b3be05053fda73c396f7459322f8294
Solaris <ovvo.system@gmail.com>
2023-09-19 10:36:45 -0300

    feat: get watchlist from AuthContext

--- client/src/routes/Profile.jsx
+++ client/src/routes/Profile.jsx
@@ -1,24 +1,15 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import AuthContext from "../contexts/AuthContext";
 import Catalogue from "../Catalogue";
 
 function Profile() {
-    const [watchlist, setWatchlist] = useState("");
     const { user } = useContext(AuthContext);
-
-    useEffect(() => {
-        fetch(`http://127.0.0.1:8000/api/watchlist/${user.user_id}/`)
-            .then((response) => response.json())
-            .then((data) => {
-                setWatchlist(data.watchlist);
-            });
-    }, [user]);
 
     return (
         <div className="flex flex-col justify-center items-center">
             <h1 className="text-3xl font-bold m-8">Hello, {user.username}</h1>
             <div className="border-t-2 border-gray-300">
-                <Catalogue movieList={watchlist} />
+                <Catalogue movieList={user.watchlist} />
             </div>
         </div>
     );
